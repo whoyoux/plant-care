905ac9fa496154bf57d2cfee3d0eef312a3f350c
whoyoux <48690153+whoyoux@users.noreply.github.com>
2024-07-01 23:46:43 +0200

    changes max duration to 2 minutes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { notFound } from "next/navigation";
 import TakePhoto from "./take-photo";
 import UploadPhoto from "./upload-photo";
 
-export const maxDuration = 5 * 64;
+export const maxDuration = 120;
 
 const DashoboardPage = async () => {
 	const session = await auth();
