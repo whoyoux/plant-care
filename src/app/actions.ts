8293903778397cea69fee2ab972669283ca3bfa7
whoyoux <48690153+whoyoux@users.noreply.github.com>
2024-07-01 03:24:35 +0200

    Update actions.ts

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,7 +3,7 @@
 import { findPlant } from "@/services/ai";
 
 export async function findPlantAction(formData: FormData) {
-	const res = await findPlant();
-	console.log(res);
+	// const res = await findPlant();
+	// console.log(res);
 	return null;
 }
